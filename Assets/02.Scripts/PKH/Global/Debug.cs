05e6ccea5d24a3ce2d09af0372b670225b19a602
박광훈 <zse638ss@gmail.com>
2023-11-23 18:05:22 +0900

    Debug.LogFormat 오버라이드 추가

--- Assets/02.Scripts/PKH/Global/Debug.cs
+++ Assets/02.Scripts/PKH/Global/Debug.cs
@@ -8,6 +8,7 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using UnityEngineInternal;
+using System.Diagnostics;
 
 
 /// 
@@ -73,18 +74,25 @@ public static class Debug
     [System.Diagnostics.Conditional("UNITY_EDITOR")] 
     public static void DrawLine(Vector3 start, Vector3 end, Color color = default(Color), float duration = 0.0f, bool depthTest = true)
     {
- 	UnityEngine.Debug.DrawLine(start, end, color, duration, depthTest);
+ 	    UnityEngine.Debug.DrawLine(start, end, color, duration, depthTest);
     } 
 	
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void DrawRay(Vector3 start, Vector3 dir, Color color = default(Color), float duration = 0.0f, bool depthTest = true)
     {
-	UnityEngine.Debug.DrawRay(start, dir, color, duration, depthTest);
+	    UnityEngine.Debug.DrawRay(start, dir, color, duration, depthTest);
     }
  	
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition)
     {
-	if (!condition) throw new Exception();
+	    if (!condition) throw new Exception();
+    }
+
+    [Conditional("UNITY_EDITOR")]
+    public static void LogFormat(string format, params object[] args)
+    {
+        string message = string.Format(format, args);
+        UnityEngine.Debug.Log(message);
     }
 }
