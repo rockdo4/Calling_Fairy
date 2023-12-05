f0fc88a8a2c5be7541120b7eaaa4c6e8a2b5bcf9
박순국 <qkrtnsrnr@gmail.com>
2023-12-06 02:21:37 +0900

    잘자고 수정했는데 아니 어떻게 해야하는거야 생성하고 삭제하는게 머리가 안돌아간다 내일 공책가져가서 수정해야지

--- Assets/Scripts/UI/InGameUI/ChainEffect.cs
+++ Assets/Scripts/UI/InGameUI/ChainEffect.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class ChainEffector
 {
-    public SkillInfo chains;
+    public SkillInfo[] chains;
     public GameObject man;
 }
 public class ChainEffect : MonoBehaviour
@@ -21,34 +22,83 @@ public class ChainEffect : MonoBehaviour
         go.onChainEffect += CheckChainEffect;
     }
 
+    //public void CheckChainEffect(SkillInfo[] chain)
+    //{
+    //    for (int i = 0; i < effectList.Count; i++) 
+    //    {
+    //        for(int j = 0; j< effectList[i].chains.Length; j++) 
+    //        {
+
+    //            if(effectList[i].chains.Contains(chain[j]))
+    //            {
+    //                //Destroy(effectList[i].man);
+    //                return;
+    //            }
+    //        }
+    //    }
+    //    //if(effectList.Count>0)
+    //    //{
+    //    //    if (effectList.Any(x => x.chains == chain))
+    //    //        return;
+    //    //}
+    //    var gameObj = Instantiate(prefab);
+    //    var newGO = new ChainEffector() { chains = chain, man = gameObj };
+    //    gameObj.transform.SetParent(transform);
+    //    effectList.Add(newGO);
+
+
+
+
+
+
+
+
+
+
+
+
+    //    //foreach (var effect in chain)
+    //    //{
+    //    //    if (!chainObjects.ContainsKey(effect))
+    //    //    {
+    //    //        var newGo = Instantiate(prefab);
+    //    //        newGo.transform.SetParent(transform);
+    //    //        chainObjects.Add(effect, newGo);
+
+    //    //    }
+    //    //    var newEffector = new ChainEffector { chains = effect, man = chainObjects[effect] };
+    //    //    for(int i = 0; i< effectList.Count; i++)
+    //    //    {
+    //    //        //if (!chainObjects.Any(effectList[i]))
+    //    //        {
+    //    //            effectList.Add(newEffector);
+    //    //        }
+
+    //    //    }
+    //    //}
+
+    //    //Debug.Log(effectList.Count);
+
+    //}
     public void CheckChainEffect(SkillInfo[] chain)
     {
-        foreach (var effect in chain)
+        for (int i = 0; i < effectList.Count; i++)
         {
-            if (!chainObjects.ContainsKey(effect))
+            if (effectList[i].chains.Length == chain.Length && effectList[i].chains.SequenceEqual(chain))
             {
-                var newGo = Instantiate(prefab);
-                newGo.transform.SetParent(transform);
-                chainObjects.Add(effect, newGo);
-
-            }
-            var newEffector = new ChainEffector { chains = effect, man = chainObjects[effect] };
-            for(int i = 0; i< effectList.Count; i++)
-            {
-                if (!chainObjects.Any(effectList[i]))
-                {
-                    effectList.Add(newEffector);
-                }
-
+                Destroy(effectList[i].man);
+                effectList.RemoveAt(i);
+                break;
             }
         }
 
-        Debug.Log(effectList.Count);
+        // 새로운 체인을 이용해 새로운 게임 오브젝트를 생성
+        var gameObj = Instantiate(prefab);
+        var newGO = new ChainEffector() { chains = chain, man = gameObj };
+        gameObj.transform.SetParent(transform);
+
+        effectList.Add(newGO);
     }
 
-    public void DeletechainEffect(SkillInfo[] chain)
-    {
-
-    }
 
 }
