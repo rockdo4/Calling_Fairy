f374eac58718fc4cc78c42ab06bd65253b49ab83
장다훈 <jangdahoon8850@gmail.com>
2023-12-08 15:55:39 +0900

    포메이션 관련 오류 수정

--- Assets/Scripts/UI/FormationSystem.cs
+++ Assets/Scripts/UI/FormationSystem.cs
@@ -13,7 +13,6 @@ public class FormationSystem : MonoBehaviour
     public void OnEnable()
     {
         CardStateInit();
-        Debug.Log("OnEnable");
     }
 
     public void CardStateInit()
--- Assets/Scripts/UI/InvUI.cs
+++ Assets/Scripts/UI/InvUI.cs
@@ -132,7 +132,7 @@ public class InvUI : UI
                         }
                         else
                         {
-                            button.enabled = card.IsUse;
+                            button.enabled = !card.IsUse;
                             button?.onClick.AddListener(() => formationSys.SortSetFairy(cardButton.inventoryItem));
                             button?.onClick.AddListener(NonActiveUI);
                         }
