19cbb4ed51bd50b1cc451395f50eea1fc65be0fb
장다훈 <jangdahoon8850@gmail.com>
2023-12-08 17:59:40 +0900

    실도

---
+++ Assets/Scripts/Skill/SkillLogic/Buffs/Shield.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : BuffBase
+{
+    public float leftshield;
+    public override void OnEnter()
+    {
+        base.OnEnter();
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnExit()
+    {        
+    }
+}
