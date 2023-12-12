d2a78f2dc3ad50860dbf9f8cd28effdf1896f540
장다훈 <jangdahoon8850@gmail.com>
2023-12-12 14:37:51 +0900

    우선 순위 변경으로 인한 배경 작업 미뤄둠 및 캐릭터 관련 에셋 다운로드

--- Assets/Scripts/Manager/BackgroundController.cs
+++ Assets/Scripts/Manager/BackgroundController.cs
@@ -3,80 +3,88 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour
 {
     private Camera mainCamera;
+    private Vector3 prevCamPos;
     //public GameObject frontBackground;
-    public GameObject[] middleBackgrounds;
-    private float spriteHalfWidth;
     public GameObject tailBackground;
-    private float tailBackgroundHalfSize;
 
     public GameObject[] farBackgrounds;
     [Tooltip("원경 따라오는 속도")]
     public float fBfollowSpeed;
-    private float farBackgroundsHalfWidth;
+    public GameObject[] middleBackgrounds;
+    [Tooltip("중경 따라오는 속도(원경보다 느려야함)")]
+    public float mBfollowSpeed;
     public GameObject[] nearBackgrounds;
-    [Tooltip("근경 따라오는 속도(원경보다 느려야함)")]
-    public float nBfollowSpeed;
-    private float nearBackgroundsHalfWidth;
 
-    private int mbCounter;
+    private float moveTo;
+
+    private int fCounter = 0;
+    private int mCounter = 0;
+    private int nCounter = 0;
     private GameObject tb;
     private CameraManager cm;
 
     private void Awake()
     {
         mainCamera = Camera.main;
-        //cm = GameObject.FindWithTag(Tags.CameraManager).GetComponent<CameraManager>();
-        //var sprite = middleBackgrounds[0].GetComponent<SpriteRenderer>().sprite;
-        //spriteHalfWidth = sprite.rect.width / sprite.pixelsPerUnit / 2f;
-        //sprite = tailBackground.GetComponent<SpriteRenderer>().sprite;
-        //tailBackgroundHalfSize = sprite.rect.width / sprite.pixelsPerUnit / 2f;
-        ////sprite = farBackgrounds[0].GetComponent<SpriteRenderer>().sprite;
-        //farBackgroundsHalfWidth = sprite.rect.width / sprite.pixelsPerUnit / 2f;
-        ////sprite = nearBackgrounds[0].GetComponent<SpriteRenderer>().sprite;
-        //nearBackgroundsHalfWidth = sprite.rect.width / sprite.pixelsPerUnit / 2f;
+        prevCamPos = mainCamera.transform.position;
     }
 
     private void Update()
     {
-        //var centerGap = middleBackgrounds[mbCounter].transform.position.x - mainCamera.transform.position.x;
-        //var rightSide = centerGap - spriteHalfWidth + (mainCamera.orthographicSize);
-        //var leftSide = centerGap + spriteHalfWidth - (mainCamera.orthographicSize);
-        //if (rightSide < 0.01)
-        //{
-        //    var pos = middleBackgrounds[mbCounter].transform.position.x;
-        //    mbCounter++;
-        //    if (mbCounter >= middleBackgrounds.Length)
-        //    {
-        //        mbCounter = 0;
-        //    }
-        //    pos += spriteHalfWidth * 3;
-        //    middleBackgrounds[mbCounter].transform.position = new Vector2(pos, 0);
-        //}
-        //if(leftSide < 0.01)
-        //{
-        //    var pos = -middleBackgrounds[mbCounter].transform.position.x;
-        //    mbCounter++;
-        //    if (mbCounter >= middleBackgrounds.Length)
-        //    {
-        //        mbCounter = 0;
-        //    }
-        //    pos -= spriteHalfWidth * 3;
-        //    middleBackgrounds[mbCounter].transform.position = new Vector2(pos, 0);
-        //}
-        //if(tb != null)
-        //{
-        //    centerGap = tb.transform.position.x - mainCamera.transform.position.x;
-        //    rightSide = centerGap - tailBackgroundHalfSize + (mainCamera.orthographicSize);
-        //    if(rightSide < 0.01)
-        //    {
-        //        cm.StopMoving();
-        //    }
-        //}
+        /*
+        var centerGap = middleBackgrounds[mbCounter].transform.position.x - mainCamera.transform.position.x;
+        var rightSide = centerGap - spriteHalfWidth + (mainCamera.orthographicSize);
+        var leftSide = centerGap + spriteHalfWidth - (mainCamera.orthographicSize);
+        if (rightSide < 0.01)
+        {
+            var pos = middleBackgrounds[mbCounter].transform.position.x;
+            mbCounter++;
+            if (mbCounter >= middleBackgrounds.Length)
+            {
+                mbCounter = 0;
+            }
+            pos += spriteHalfWidth * 3;
+            middleBackgrounds[mbCounter].transform.position = new Vector2(pos, 0);
+        }
+        if (leftSide < 0.01)
+        {
+            var pos = -middleBackgrounds[mbCounter].transform.position.x;
+            mbCounter++;
+            if (mbCounter >= middleBackgrounds.Length)
+            {
+                mbCounter = 0;
+            }
+            pos -= spriteHalfWidth * 3;
+            middleBackgrounds[mbCounter].transform.position = new Vector2(pos, 0);
+        }
+        if (tb != null)
+        {
+            centerGap = tb.transform.position.x - mainCamera.transform.position.x;
+            rightSide = centerGap - tailBackgroundHalfSize + (mainCamera.orthographicSize);
+            if (rightSide < 0.01)
+            {
+                cm.StopMoving();
+            }
+        }
+        */
+        moveTo = prevCamPos.x - mainCamera.transform.position.x;
+        prevCamPos = mainCamera.transform.position;
+        foreach (var item in farBackgrounds)
+        {
+            item.transform.position += new Vector3(moveTo * fBfollowSpeed, 0);
+        }
+        foreach (var item in middleBackgrounds)
+        {
+            item.transform.position += new Vector3(moveTo * mBfollowSpeed, 0);
+        }
+        CheckSide(farBackgrounds, fCounter);
+        CheckSide(middleBackgrounds, mCounter);
+        CheckSide(nearBackgrounds, nCounter);
     }
 
     public void SetTailBackground()
     {
-        var pos = middleBackgrounds[mbCounter].transform.position.x;
+        var pos = nearBackgrounds[nCounter].transform.position.x;
         pos += tailBackground.GetComponent<SpriteRenderer>().sprite.rect.width / 200 * 3;
         tb = Instantiate(tailBackground, new Vector3(pos, 0), Quaternion.identity);
     }
@@ -86,8 +94,13 @@ public class BackgroundController : MonoBehaviour
         tb.AddComponent<TailBackground>();
     }
 
-    private void CheckSide(GameObject backgound, float halfSize)
+    private void CheckSide(GameObject[] backgrounds, int counter)
     {
+        var centerGap = backgrounds[counter].transform.position.x - mainCamera.transform.position.x;
+        var sideSize = mainCamera.orthographicSize * mainCamera.aspect;
+        if (centerGap > sideSize)
+        {
 
+        }
     }
 }
