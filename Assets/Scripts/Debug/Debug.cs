8ba843f37233cd9fc40204942aa205f834e1c3e1
장다훈 <jangdahoon8850@gmail.com>
2023-12-20 14:17:38 +0900

    버그 해결 - 스케일 오류
    초기 세팅에서 스케일을 초기화 하여 해결

--- Assets/Scripts/Debug/Debug.cs
+++ Assets/Scripts/Debug/Debug.cs
@@ -24,65 +24,65 @@ using UnityEngine;
 /// 
 /// 2012.11. @kimsama
 /// 
-public static class Debug 
+public static class Debug
 {
     public static bool isDebugBuild
     {
-	get { return UnityEngine.Debug.isDebugBuild; }
+        get { return UnityEngine.Debug.isDebugBuild; }
     }
 
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
-    public static void Log (object message)
-    {   
-        UnityEngine.Debug.Log (message);
+    public static void Log(object message)
+    {
+        UnityEngine.Debug.Log(message);
     }
 
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
-    public static void Log (object message, UnityEngine.Object context)
-    {   
-        UnityEngine.Debug.Log (message, context);
+    public static void Log(object message, UnityEngine.Object context)
+    {
+        UnityEngine.Debug.Log(message, context);
     }
-		
+
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
-    public static void LogError (object message)
-    {   
-        UnityEngine.Debug.LogError (message);
+    public static void LogError(object message)
+    {
+        UnityEngine.Debug.LogError(message);
     }
 
-    [System.Diagnostics.Conditional("UNITY_EDITOR")]	
-    public static void LogError (object message, UnityEngine.Object context)
-    {   
-        UnityEngine.Debug.LogError (message, context);
-    }
- 
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
-    public static void LogWarning (object message)
-    {   
-        UnityEngine.Debug.LogWarning (message.ToString ());
+    public static void LogError(object message, UnityEngine.Object context)
+    {
+        UnityEngine.Debug.LogError(message, context);
     }
 
-    [System.Diagnostics.Conditional("UNITY_EDITOR")] 
-    public static void LogWarning (object message, UnityEngine.Object context)
-    {   
-        UnityEngine.Debug.LogWarning (message.ToString (), context);
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarning(object message)
+    {
+        UnityEngine.Debug.LogWarning(message.ToString());
     }
 
-    [System.Diagnostics.Conditional("UNITY_EDITOR")] 
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarning(object message, UnityEngine.Object context)
+    {
+        UnityEngine.Debug.LogWarning(message.ToString(), context);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void DrawLine(Vector3 start, Vector3 end, Color color = default(Color), float duration = 0.0f, bool depthTest = true)
     {
- 	    UnityEngine.Debug.DrawLine(start, end, color, duration, depthTest);
-    } 
-	
+        UnityEngine.Debug.DrawLine(start, end, color, duration, depthTest);
+    }
+
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void DrawRay(Vector3 start, Vector3 dir, Color color = default(Color), float duration = 0.0f, bool depthTest = true)
     {
-	    UnityEngine.Debug.DrawRay(start, dir, color, duration, depthTest);
+        UnityEngine.Debug.DrawRay(start, dir, color, duration, depthTest);
     }
- 	
+
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition)
     {
-	    if (!condition) throw new Exception();
+        if (!condition) throw new Exception();
     }
 
     [Conditional("UNITY_EDITOR")]
