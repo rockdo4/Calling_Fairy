67a61a2292390f695cd119cd65a4a3c0781096b1
박순국 <qkrtnsrnr@gmail.com>
2023-12-27 14:20:37 +0900

    <수정사항> 버튼 OnEnable 할때 리스트 갱신되도록 함.

--- Assets/Scripts/UI/MainLobby/FirstTownCharSetting.cs
+++ Assets/Scripts/UI/MainLobby/FirstTownCharSetting.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 public class FirstTownCharSetting : MonoBehaviour
 {
     //public AnimatorController animatorController;
-
+    private SettingUI firstCharSet;
     private void Awake()
     {
-        var firstCharSet = GameObject.FindWithTag(Tags.Canvas).GetComponentInChildren<SettingUI>(true);
-        firstCharSet.FirstTownSetting();
-
+        firstCharSet = GameObject.FindWithTag(Tags.Canvas).GetComponentInChildren<SettingUI>(true);
+        SettingCharSet();
     }
-
+    public void SettingCharSet()
+    {
+        firstCharSet.FirstTownSetting();
+    }
+    
 
 }
